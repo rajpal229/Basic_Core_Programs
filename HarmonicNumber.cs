c1c4a5d414a91575235f3f72797b4702db6929df
rajpal229 <rajpalrawat229@gmail.com>
2022-12-08 11:22:02 +0530

    Added: UC4_HarmonicNumber

---
+++ HarmonicNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Programs
+{
+    internal class HarmonicNumber
+    {
+        public void nthHarmonicNumber()
+        {
+            Console.WriteLine("Enter Number");
+            int N = Convert.ToInt32(Console.ReadLine());
+            float Harmonic_Value = 0;
+            for (float i = 1; i <= N; i++)
+            {
+                Harmonic_Value = Harmonic_Value + 1 / i;
+            }
+            Console.WriteLine(N + " th Harmonic Value is " + Harmonic_Value);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@
             bool Flag = true;
             while (Flag)
             {
-                Console.WriteLine("Enter:\n 1. Flip Coin \n 2. Leap Year\n3. Power of 2");
+                Console.WriteLine("Enter:\n 1. Flip Coin \n 2. Leap Year\n3. Power of 2\n4. Harmonic Number");
                 int Option = Convert.ToInt32(Console.ReadLine());
                 switch (Option)
                 {
@@ -22,6 +22,14 @@
                     case 3:
                         PowerOfTwo powerofTwo = new PowerOfTwo();
                         powerofTwo.powerofTwo();
+                        break;
+                    case 4:
+                        HarmonicNumber harmonicNumber = new HarmonicNumber();
+                        harmonicNumber.nthHarmonicNumber();
+                        break;
+                    case 5:
+                        PrimeFactors factors = new PrimeFactors();
+                        factors.Prime_Factors();
                         break;
                     default:
                         Console.WriteLine("Enter Valid Number");
