90ddf530bf5758a0fc312f0776a9ccf8fba40f02
rajpal229 <rajpalrawat229@gmail.com>
2022-12-08 11:37:49 +0530

    Added: UC9_VowelConsonant

---
+++ VowelConsonant.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Programs
+{
+    internal class VowelConsonant
+    {
+        public void CheckVowelorConsanant()
+        {
+            Console.WriteLine("Enter Alphabet");
+            char Alphabet = Console.ReadKey().KeyChar;
+            switch (Alphabet)
+            {
+                case 'a':
+                    Console.WriteLine("\n" + Alphabet + " is Vowel");
+                    break;
+                case 'e':
+                    Console.WriteLine("\n" + Alphabet + " is Vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine("\n" + Alphabet + " is Vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine("\n" + Alphabet + " is Vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine("\n" + Alphabet + " is Vowel");
+                    break;
+                default:
+                    Console.WriteLine("\n" + Alphabet + " is Consonant");
+                    break;
+            }
+        }
+    }
+}
